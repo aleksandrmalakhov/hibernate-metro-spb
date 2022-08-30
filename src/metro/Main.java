18b20b67708a8c2d399f85c97cf9c445a8df0142
Aleksandr Malakhov <schwarcroza@mail.ru>
2022-08-30 11:14:25 +0300

    update

--- src/metro/Main.java
+++ src/metro/Main.java
@@ -1,14 +1,20 @@
 package metro;
 
 import lombok.NonNull;
-import metro.core.*;
+import metro.core.Line;
+import metro.core.Station;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.*;
-import org.hibernate.boot.*;
-import org.hibernate.boot.registry.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.*;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     private static Logger logger;
